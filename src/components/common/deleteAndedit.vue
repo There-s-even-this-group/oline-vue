72986adc4ece255d6a0153fb17dafbe7b70e9d94
wonichan <1012403910@qq.com>
2020-02-07 19:14:38 +0800

    2/7托福人发布页完成

---
+++ src/components/common/deleteAndedit.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="delete-edit">
+    <slot name="delete">
+      <a id="delete" >删除</a>
+    </slot>
+    <slot name="edit">
+      <a id="edit" >编辑</a>
+    </slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "deleteAndedit"
+  }
+</script>
+
+<style scoped>
+  .delete-edit{
+    display: flex;
+  }
+</style>
