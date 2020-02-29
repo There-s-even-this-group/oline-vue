21bcd5aa049525bca213743e9ab388dee993fb22
wuyue <17184030601@163.com>
2020-03-01 01:08:18 +0800

    个人信息修改功能完成

--- src/components/个人资料/personComp/baseinf.vue
+++ src/components/个人资料/personComp/baseinf.vue
@@ -1,7 +1,7 @@
 <template>
-    <el-form ref="userInf" :model="userInf" :rules="roles" label-width="85px" style="width: 30%;margin-top: 30px" >
+    <el-form ref="userInf" :model="userInf" :rules="roles" label-width="85px" style="width: 30%;margin-top: 30px">
         <el-form-item label="用户名：">
-            <el-input v-model="userInf.name" :disabled="true"></el-input>
+            <el-input v-model="userInf.username" :disabled="true"></el-input>
         </el-form-item>
         <el-form-item label="性别：">
             <el-radio-group v-model="userInf.sex" style="margin-left: 10%">
@@ -9,18 +9,18 @@
                 <el-radio label="女"></el-radio>
             </el-radio-group>
         </el-form-item>
+        <el-form-item label="出生日期">
+            <el-col :span="11">
+                <el-form-item>
+                    <el-date-picker type="date" placeholder="选择日期" v-model="userInf.birthdata" style="width: 219%;"></el-date-picker>
+                </el-form-item>
+            </el-col>
+        </el-form-item>
         <el-form-item label="邮箱：" prop="email">
             <el-input v-model="userInf.email"></el-input>
         </el-form-item>
         <el-form-item label="电话：" prop="phone">
-            <el-input v-model="userInf.phone"></el-input>
-        </el-form-item>
-        <el-form-item label="出生日期">
-            <el-col :span="11">
-                <el-form-item>
-                    <el-date-picker type="date" placeholder="选择日期" v-model="userInf.birthdata" style="width: 100%;"></el-date-picker>
-                </el-form-item>
-            </el-col>
+            <el-input v-model="userInf.phone" placeholder="请输入电话"></el-input>
         </el-form-item>
         <el-form-item label="个性签名：">
             <el-input
@@ -34,7 +34,7 @@
             <span style="font-size: 3px;color: #a2a2a2">请输入0-100个字符</span>
         </el-form-item>
         <el-form-item>
-            <el-button type="primary" style="width: 200px">保存</el-button>
+            <el-button type="primary" style="width: 200px" @click="saveInf" :loading="loading">保存</el-button>
         </el-form-item>
     </el-form>
 </template>
@@ -53,24 +53,25 @@
             var validateEmail = (rule, value, callback) => {
                 if (value === '') {
                 } else {
-                    if (!(/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(this.userInf.phone))) {
+                    if (!(/^[A-Za-z0-9]+([_\.][A-Za-z0-9]+)*@([A-Za-z0-9\-]+\.)+[A-Za-z]{2,6}$/.test(this.userInf.email))) {
                         callback(new Error('邮箱格式错误'))
                     }
                 }
             };
             return {
                 userInf:{
-                    name: '',
+                    username: '',
                     sex: '',
                     email: '',
                     phone: '',
-                    birthdata: '',
+                    birthdata: ' ',
                     sign: ''
                 },
                 roles:{
                     email:[{ validator: validateEmail, trigger: 'blur'}],
                     phone:[{ validator: validatePhone, trigger: 'blur'}]
-                }
+                },
+                loading: false
             }
         },
         mounted: function () {
@@ -79,15 +80,44 @@
         methods: {
             init(){
                 this.$store.dispatch('user/getInfo');
-                this.userInf.name = this.$store.getters.username;
+                this.userInf.username = this.$store.getters.username;
                 this.userInf.sex = this.$store.getters.sex;
                 this.userInf.email = this.$store.getters.email;
-                if (this.$store.getters.phone != 0){
-                    this.userInf.phone = this.$store.getters.phone;
-                }
+                this.userInf.phone = this.$store.getters.phone;
                 this.userInf.birthdata = this.$store.getters.birthdata;
                 this.userInf.sign = this.$store.getters.sign;
-                console.log(this.userInf)
+            },
+            saveInf(){
+                this.loading = true;
+                this.userInf.birthdata = this.formattingDate(this.userInf.birthdata);
+                this.postRequest('/changInf',this.userInf).then(resp => {
+                    this.loading = false;
+                    var data = resp.data;
+                    if (data.code === 200) {
+                        this.$message({
+                            message: data.message,
+                            type: 'success'
+                        })
+                    } else {
+                        this.$message({
+                            message: data.message,
+                            type: 'error'
+                        })
+                    }
+                }).catch(error => {
+                    this.loading = false;
+                    this.$message({
+                        message: error,
+                        type: 'error'
+                    })
+                })
+            },
+            formattingDate(date){
+                date= new Date(Date.parse(date));
+                var y = date.getFullYear();
+                var m = date.getMonth()+1;
+                var d = date.getDate();
+                return y+'-'+m+'-'+d;
             }
         }
     }
